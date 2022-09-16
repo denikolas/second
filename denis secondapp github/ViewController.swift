e03566c8530e9e3e8ae4c167a232efc9d1675cd8
denis <pusika97@gmail.com>
2022-09-16 13:41:05 +0300

    Initial Commit

---
+++ denis secondapp github/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  denis secondapp github
+//
+//  Created by user on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
